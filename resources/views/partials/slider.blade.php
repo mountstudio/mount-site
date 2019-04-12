f6f4e574ab3afa5e022e90cf7de7eb4fd23ff299
Bainurs <nurs.axe@gmail.com>
2019-04-12 13:17:37 +0600

    slider and card info

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -2,17 +2,17 @@
 <section class="welcome-area">
     <div class="welcome-slides owl-carousel">
         <!-- Single Slide -->
-        <div class="single-welcome-slide bg-img bg-overlay" style="background-image: url({{ asset('img/bg-img/1.jpg') }});">
+        <div class="single-welcome-slide bg-img bg-overlay" style="background-image: url({{ asset('img/bg-img/kenny.jpg') }});">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <!-- Welcome Text -->
                     <div class="col-12 col-lg-8 col-xl-6">
                         <div class="welcome-text">
-                            <h2 data-animation="bounceInDown" data-delay="900ms">Hello <br>I'm Jackson</h2>
-                            <p data-animation="bounceInDown" data-delay="500ms">I photograph very instinctively. I see how it is taken like that. I do not follow certain styles, philosophies or teachers.</p>
+                            <h2 data-animation="bounceInDown size-h2" data-delay="900ms">Hello <br>I'm Erkin</h2>
+                            <p data-animation="bounceInDown" data-delay="500ms">I programmer very instinctively. I see how it is taken like that. I do not follow certain styles, philosophies or teachers.</p>
                             <div class="hero-btn-group" data-animation="bounceInDown" data-delay="100ms">
                                 <a href="#" class="btn alime-btn mb-3 mb-sm-0 mr-4">Get a Quote</a>
-                                <a class="hero-mail-contact" href="mailto:hello.alime@gmail.com">hello.alime@gmail.com</a>
+                                <a class="hero-mail-contact" href="mailto:nurs.axe@gmail.com">mount.studio@gmail.com</a>
                             </div>
                         </div>
                     </div>
@@ -21,17 +21,17 @@
         </div>
 
         <!-- Single Slide -->
-        <div class="single-welcome-slide bg-img bg-overlay" style="background-image: url({{ asset('img/bg-img/2.jpg') }});">
+        <div class="single-welcome-slide bg-img bg-overlay" style="background-image: url({{ asset('img/bg-img/kenny2.jpg') }});">
             <div class="container h-100">
                 <div class="row h-100 align-items-center">
                     <!-- Welcome Text -->
                     <div class="col-12 col-lg-8 col-xl-6">
                         <div class="welcome-text">
-                            <h2 data-animation="bounceInUp" data-delay="100ms">Hello <br>I'm Alime</h2>
-                            <p data-animation="bounceInUp" data-delay="500ms">I photograph very instinctively. I see how it is taken like that. I do not follow certain styles, philosophies or teachers.</p>
+                            <h2 data-animation="bounceInUp size-h2" data-delay="100ms">Привет Я работающий Эркин</h2>
+                            <p data-animation="bounceInUp" data-delay="500ms">Я работаю backend программистом в mount-studio. Работал над большими проектами и когда мы работаем мы лишь концентрируемся на нашей работе. У меня есть свой стиль написания кода, Эркин это Ержан.</p>
                             <div class="hero-btn-group" data-animation="bounceInUp" data-delay="900ms">
                                 <a href="#" class="btn alime-btn mb-3 mb-sm-0 mr-4">Get a Quote</a>
-                                <a class="hero-mail-contact" href="mailto:hello.alime@gmail.com">hello.alime@gmail.com</a>
+                                <a class="hero-mail-contact" href="mailto:tilek.kubanov@gmail.com">mount-studio@gmail.com</a>
                             </div>
                         </div>
                     </div>
--- resources/views/work/index.blade.php
+++ resources/views/work/index.blade.php
@@ -28,4 +28,42 @@
         </div>
     </div>
 </div>
+
+<div class="container py-5">
+    <div class="row">
+        <div class="col-12 text-center pb-5">
+            <span class="color-style h2"> Мы предоставим вам </span>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('img/icons/data.png') }}" class="card-img-top" alt="Разработка сайтов">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="/public/img/icon/contract.png" class="card-img-top" alt="Заключение договора">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="/public/img/icon/seo.png" class="card-img-top" alt="СЕО оптимизация">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Gallery Area End -->
